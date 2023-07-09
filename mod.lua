503043c84e6a3d300ad02de232898769b008639a
Florian Thiel <flth@flth.de>
2023-07-09 14:02:19 +0200

    initial

---
+++ mod.lua
@@ -0,0 +1,58 @@
+﻿function data()
+return {
+	info = {
+		minorVersion = 0,
+		severityAdd = "NONE",
+		severityRemove = "WARNING",
+		name = _("mod_name"),
+		description = _("mod_desc"),
+		authors = {
+		    {
+		        name = "ModWerkstatt",
+		        role = "CREATOR",
+		    },
+		},
+		tags = { "europe", "deutschland", "germany", "schweiz", "db", "waggon", "goods", },
+		minGameVersion = 0,
+		dependencies = { },
+		url = { "" },
+
+        params = {
+			{
+				key = "megaiifake",
+				name = _("Fake_mega_ii_wagen"),
+				values = { "No", "Yes", },
+				tooltip = _("option_fake_mega_ii_desc"),
+				defaultIndex = 0,
+			},
+        },
+	},
+	options = {
+	},
+	
+	runFn = function (settings, modParams)
+	
+        local hidden = {
+			["1989_fake.mdl"] = true,
+			["1998_fake.mdl"] = true,
+			["2003_fake.mdl"] = true,
+            ["2004_fake.mdl"] = true,   
+        }
+
+		local modelFilter = function(fileName, data)
+			local modelName = fileName:match('/mega_ii_([^/]*.mdl)')
+			return (modelName==nil or hidden[modelName]~=true)
+		end
+
+        if modParams[getCurrentModId()] ~= nil then
+			local params = modParams[getCurrentModId()]
+			if params["megaiifake"] == 0 then
+				addFileFilter("model/vehicle", modelFilter)
+			end
+		else
+			addFileFilter("model/vehicle", modelFilter)
+		end
+
+	end
+	}
+end
---
+++ strings.lua
@@ -0,0 +1,64 @@
+function data()
+return {
+	en = {
+
+		["mod_name"] = "Mega II Sdggmrrs(s)",
+		["mod_desc"] = "This mod contains a series of four-axle freight wagons, the first of which were put into service at Hupac from 1989. In 2003 a new series of Mega II double pocket wagons was built. In 2004 another series was built with Y31 bogies and disc brakes.",
+        
+        ["Fake_mega_ii_wagen"] = "Activate Mega II fake versions?",
+        ["option_fake_mega_ii_desc"] = "Should the fake versions of the Mega II be available in the game?",
+		
+        ["menu_mega_ii"] = "Mega II Sdggmrrs(s)",
+
+        ["mega_ii_1989_name"] = "Mega II Sdggmrrs (1989)",
+        ["mega_ii_1989_desc"] = "Wagon combination consisting of a wagon for trailers/containers and a wagon for containers only. With container loading before 1998.",
+		
+        ["mega_ii_1998_name"] = "Mega II Sdggmrrs (1998)",
+        ["mega_ii_1998_desc"] = "Wagon combination consisting of a wagon for trailers/containers and a wagon for containers only. With container loading from 1998.",
+		
+        ["mega_ii_2003_name"] = "Mega II Sdggmrrs (2003)",
+        ["mega_ii_2003_desc"] = "Wagon combination of two wagons for trailers/containers. With container loading from 1998.",
+		
+        ["mega_ii_2004_name"] = "Mega II Sdggmrrss (2004)",
+        ["mega_ii_2004_desc"] = "Wagon combination consisting of a wagon for trailers/containers and a wagon for containers only. The combination has a Y31 bogie with disc brakes and is designed for 120km/h. With container loading from 1998.", 
+		
+		
+		["menu_mega_ii_fake"] = "Mega II Sdggmrrs(s) Fake",
+		["mega_ii_1989_fake_name"] = "Mega II Sdggmrrs (1989) Fake",		
+        ["mega_ii_1998_fake_name"] = "Mega II Sdggmrrs (1998) Fake",		
+        ["mega_ii_2003_fake_name"] = "Mega II Sdggmrrs (2003) Fake",		
+        ["mega_ii_2004_fake_name"] = "Mega II Sdggmrrss (2004) Fake",
+
+	},
+	de = {
+
+		["mod_name"] = "Mega II Sdggmrrs(s)",
+		["mod_desc"] = "Dieser Mod enthält eine Reihe von vierachsigen Güterwagen, von denen die ersten ab 1989 bei der Hupac in Dienst gestellt wurden. Im Jahr 2003 wurde eine neue Serie der Mega II mit Doppeltaschenwagen gebaut. 2004 wurde eine weitere Serie mit Y31 Drehgestellen und Scheibenbremse gebaut.",
+        
+        ["Fake_mega_ii_wagen"] = "Mega II Fake-Versionen aktivieren?",
+        ["option_fake_mega_ii_desc"] = "Sollen die Fake-Versionen der Mega II verfügbar sein im Spiel?",
+		
+        ["menu_mega_ii"] = "Mega II Sdggmrrs(s)",
+
+        ["mega_ii_1989_name"] = "Mega II Sdggmrrs (1989)",
+        ["mega_ii_1989_desc"] = "Wagenkombination aus einem Wagen für Trailer/Container und einem Wagen nur für Container. Mit Container Beladung vor 1998.",
+		
+        ["mega_ii_1998_name"] = "Mega II Sdggmrrs (1998)",
+        ["mega_ii_1998_desc"] = "Wagenkombination aus einem Wagen für Trailer/Container und einem Wagen nur für Container. Mit Container Beladung ab 1998.",
+		
+        ["mega_ii_2003_name"] = "Mega II Sdggmrrs (2003)",
+        ["mega_ii_2003_desc"] = "Wagenkombination aus zwei Wagen für Trailer/Container. Mit Container Beladung ab 1998.",
+		
+        ["mega_ii_2004_name"] = "Mega II Sdggmrrss (2004)",
+        ["mega_ii_2004_desc"] = "Wagenkombination aus einem Wagen für Trailer/Container und einem Wagen nur für Container. Die Kombination besitzt Y31 Drehgestell mit Scheibenbremse und ist für 120km/h ausgelegt. Mit Container Beladung ab 1998.", 
+		
+		
+		["menu_mega_ii_fake"] = "Mega II Sdggmrrs(s) Fake",
+		["mega_ii_1989_fake_name"] = "Mega II Sdggmrrs (1989) Fake",		
+        ["mega_ii_1998_fake_name"] = "Mega II Sdggmrrs (1998) Fake",		
+        ["mega_ii_2003_fake_name"] = "Mega II Sdggmrrs (2003) Fake",		
+        ["mega_ii_2004_fake_name"] = "Mega II Sdggmrrss (2004) Fake",
+
+	},
+}
+end
